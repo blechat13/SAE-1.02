2a5b05d3178f1b37b8368b3f54b564a493c07de4
blechat13 <148314353+blechat13@users.noreply.github.com>
2025-12-03 15:42:09 +0100

    Update RechercheLinéaire.java

--- RechercheLinéaire.java
+++ RechercheLinéaire.java
@@ -1,3 +1,14 @@
 public class RechercheLinéaire {
-    
+    public static Boolean RechercheLinéaire (int[] tab, int elem) {
+		int i, iteration = 0;
+		for (i=0; i < tab.length; i++) {
+			if (tab[i]==elem) {
+				//System.out.println("Itérations : " + iteration);
+				return true;
+			}
+			iteration++;
+		}
+		//System.out.println("Itérations : " + iteration);
+		return false;
+		}
 }
