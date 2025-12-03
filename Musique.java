7dab370015c98f96680b1db56a191e5b191e943c
blechat13 <148314353+blechat13@users.noreply.github.com>
2025-12-03 16:29:16 +0100

    Update Musique.java

--- Musique.java
+++ Musique.java
@@ -1,5 +1,11 @@
 public class Musique {
     String titre;
     String album;
-    
+    int popularite;
+
+    public Musique (String t,String a,int p){
+        titre = t;
+        album = a;
+        popularite = p;
+    }
 }
