a9012a4d40570e04643378a316108a0ca66f1019
blechat13 <148314353+blechat13@users.noreply.github.com>
2025-12-09 18:37:50 +0100

    Update AppliMusique.java

--- AppliMusique.java
+++ AppliMusique.java
@@ -2,5 +2,15 @@
 public class AppliMusique {
     public static void main(String[] args){
         
+        List<Musique> listeMusique = new ArrayList<>();
+
+
+        BufferedReader Musique = new BufferedReader(new InputStreamReader(spotify_100.csv));
+
+        Musique m = new Musique("caca", "trou" ,150000000);
+
+        listeMusique.add(m);
+
+        System.out.println(listeMusique.get(0));
     }
 }
